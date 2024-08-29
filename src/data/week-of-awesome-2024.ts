c5d93c1c0b9e82245da9c485de608a275c7190f0
Matthew M-B <matthew.mb@getjobber.com>
2024-08-29 00:37:59 -0400

    Add updated question info

--- src/data/week-of-awesome-2024.ts
+++ src/data/week-of-awesome-2024.ts
@@ -3,8 +3,8 @@ import { Question } from "./types";
 const question1: Question = {
   id: "week-of-awesome-2024-1",
   title: "What is 1+1?",
-  content: "2",
-  answer: "test",
+  content: "Please answer this very difficult question.",
+  answer: "2",
   startDate: new Date(), // set start date to tomorrow
   endDate: new Date(new Date().getTime() + 24 * 60 * 60 * 1000), // set end date to tomorrow
 };
