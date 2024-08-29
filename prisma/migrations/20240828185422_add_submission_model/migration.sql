e4b99dc26aa19180afb0f9178ed12e05416f2e2a
Matthew M-B <matthew.mb@getjobber.com>
2024-08-29 03:20:53 -0400

    Update db to postgres

--- prisma/migrations/20240828185422_add_submission_model/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Submission" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "questionId" INTEGER NOT NULL,
-    "answer" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
