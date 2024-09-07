221d55b2e49f209da3e281bda2c947f1192e2489
Matthew M-B <matthew.mb@getjobber.com>
2024-09-07 15:10:53 -0400

    Update discord webhook

--- src/lib/discord.ts
+++ src/lib/discord.ts
@@ -19,7 +19,7 @@ const TYPE_TO_EMOJI = {
 
 export const sendAlert = async (message: DiscordMessage) => {
   const alertWebhook =
-    "https://discord.com/api/webhooks/1278553786675363984/4eWeYdvqlxJJjV7s5Y3VQpWh_EZkeIBpgBw20Ov3y1OgBGRKJkXFpTEvJUpJ0Xcw-QPw";
+    "https://discord.com/api/webhooks/1282054916797501520/nP9e25-C92uG5O58aLOdZHMNAJBU8kVb2GQJFRf4NUHqLmoEkLJJjNEn1NhbHHiiOwbV";
   const emoji = TYPE_TO_EMOJI[message.type];
   try {
     const jsonFormatted = JSON.stringify(message, null, 2);
