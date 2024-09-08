2565da2ccac18d60dc6377c0aa477532617be43c
Your Name <you@example.com>
2024-09-07 20:55:19 -0400

    add first question

--- src/data/week-of-awesome-2024.test.ts
+++ src/data/week-of-awesome-2024.test.ts
@@ -12,8 +12,31 @@ describe("Week of Awesome 2024 Questions", () => {
       (question) => question.id === "week-of-awesome-2024-1",
     ) as Question;
 
-    it("will always evaluate correctly", () => {
-      expect(question.evaluate("any string")).toBe(true);
+    const answer = "[58, 19, 35, 6, 31, 80, 44, 83, 92, 8, 56, 37, 88, 87, 14, 32, 89, 21, 4, 91, 33, 90, 17, 84, 42, 97, 38, 94, 28, 64, 66, 48, 95, 51, 60, 30, 74, 15, 96, 82, 100, 75, 10, 26, 22, 49, 81, 11, 76, 20, 98, 69, 7, 68, 46, 86, 78, 47, 73, 13, 9, 36, 40, 99, 55, 77, 23, 1, 24, 2, 53, 71, 62, 52, 3, 50, 79, 65, 57, 70, 27, 29, 16, 34, 43, 59, 61, 45, 5, 85, 25, 12, 93, 54, 67, 63, 18, 41, 39, 72]";
+
+    const differentOrderAnswer = "[19, 58, 35, 6, 31, 80, 44, 83, 92, 8, 56, 37, 88, 87, 14, 32, 89, 21, 4, 91, 33, 90, 17, 84, 42, 97, 38, 94, 28, 64, 66, 48, 95, 51, 60, 30, 74, 15, 96, 82, 100, 75, 10, 26, 22, 49, 81, 11, 76, 20, 98, 69, 7, 68, 46, 86, 78, 47, 73, 13, 9, 36, 40, 99, 55, 77, 23, 1, 24, 2, 53, 71, 62, 52, 3, 50, 79, 65, 57, 70, 27, 29, 16, 34, 43, 59, 61, 45, 5, 85, 25, 12, 93, 54, 67, 63, 18, 41, 39, 72]";
+
+    const wrongAnswer = "[0]";
+
+    const invalidFormatAnswer1 = "['1', 2, 3, 4, 5, 6, 7, 8, 9, 10]";
+
+    const invalidFormatAnswer2 = "text here";
+
+    it("will be successful given the right answer", () => {
+      expect(question.evaluate(answer)).toBe(true);
+    });
+
+    it("will be successful given the right answer in a different order", () => {
+      expect(question.evaluate(differentOrderAnswer)).toBe(true);
+    });
+
+    it("will fail given the wrong answer", () => {
+      expect(question.evaluate(wrongAnswer)).toBe(false);
+    });
+
+    it("will fail given an answer for an invalid format", () => {
+      expect(question.evaluate(invalidFormatAnswer1)).toBe(false);
+      expect(question.evaluate(invalidFormatAnswer2)).toBe(false);
     });
   });
 });
